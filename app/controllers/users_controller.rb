f887ba0a10093554525600461ab785aac4d54c90
Geoff Crittenden <geoffcritt@hotmail.com>
2014-05-28 08:25:02 -0500

    users show routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    user = User.create(email: params[:user][:email], first_name: params[:user][:first_name], last_name: params[:user][:last_name], location: params[:user][:location])
+    puts user.id
+    redirect_to "/users/#{user.id}"
+  end
+
+  def show
+    @id = params[:id]
+    render
   end
 end
