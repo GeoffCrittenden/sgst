0188b130cbb370546b49275b01780f3891f51cfa
Geoff Crittenden <geoffcritt@hotmail.com>
2014-05-28 09:50:59 -0500

    added ability to create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,6 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(owner_id: 1, suggestion_id: params[:suggestion_id], body: params[:comment][:body], vote: params[:comment][:vote])
+    redirect_to "/suggestions/#{@comment.suggestion_id}"
+  end
 end
