fda5d907f12859b3eb27fb65ad0bcf76f577cbd7
Geoff Crittenden <geoffcritt@hotmail.com>
2014-05-27 22:38:07 -0500

    changed Faker::Lorem.sentences to Faker::Lorem.paragraph

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 100.times do |n|
-  User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, location: Faker::Address.city, Faker::Address.state_abbr)  
-  Suggestion.create(owner_id: n + 1, target: Faker::Company.name, title: Faker::Lorem.sentence, body: Faker::Lorem.sentences, score: rand(1..100), local: [true,false].shuffle[0])
-  Comment.create(owner_id: rand(1..100), suggestion_id: rand(1..100), vote: [1,-1].shuffle[0], body: Faker::Lorem.sentences)
+  User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}")
+  Suggestion.create(owner_id: n + 1, target: Faker::Company.name, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, score: rand(1..100), local: [true,false].shuffle[0])
+  Comment.create(owner_id: rand(1..100), suggestion_id: rand(1..100), vote: [1,-1].shuffle[0], body: Faker::Lorem.paragraph)
 end
