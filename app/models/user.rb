fe7c583e5e839a2ca5f652ec91b72cada304e56b
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-02 16:14:56 -0500

    added secure password methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_secure_password
   has_many :suggestions
   has_many :comments
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 end
