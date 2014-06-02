d81064c83756465b97167bfe776b6bbf4574eab9
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-02 15:28:35 -0500

    created migration for User secure password field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528015540) do
+ActiveRecord::Schema.define(version: 20140602202516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140528015540) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
