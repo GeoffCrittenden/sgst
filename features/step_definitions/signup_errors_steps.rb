dd4e1c173c2ea5584b047c73f2926071139f1cd1
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-06 12:20:03 -0500

    added scenario for checking for invalid email address

--- features/step_definitions/signup_errors_steps.rb
+++ features/step_definitions/signup_errors_steps.rb
@@ -1,5 +1,5 @@
-When(/^a user submits an invalid signup form$/) do
-  @user = User.create(email:'email',
+When(/^a user submits a blank signup form$/) do
+  @user = User.create(email:'',
                       first_name: '',
                       last_name: '',
                       location: '',
@@ -7,10 +7,32 @@ When(/^a user submits an invalid signup form$/) do
                       password_confirmation: '')
 end
 
+Then(/^no account is created$/) do
+  User.find_by(email: '').should == nil
+end
+
+Then(/^they receive error messages$/) do
+  @user.errors.empty?.should == false
+end
+
+When(/^a user submits an invalid email address$/) do
+  visit('/')
+  fill_in 'user_email', :with => 'email'
+  click_button('Create Account')
+  user = { email: 'email',
+           first_name: 'first',
+           last_name: 'Last',
+           location: 'Smallville, KS',
+           password: 'password',
+           password_confirmation: 'password' }
+  user.each { |field,value| fill_in "user_#{field}", :with => value }
+  click_button('Create Your Account')
+end
+
 Then(/^their account is not created$/) do
   User.find_by(email: 'email').should == nil
 end
 
 Then(/^they receive an error message$/) do
-  @user.errors.empty?.should == false
+  page.should have_content 'is invalid'
 end
--- features/tests/signup_errors.feature
+++ features/tests/signup_errors.feature
@@ -3,7 +3,12 @@ Feature: user receives errors when incorrectly filling out signup form
   The website
   Should inform them with error messages
 
-  Scenario: if a user enters blank or invalid info
-    When a user submits an invalid signup form
+  Scenario: if a user enters blank info
+    When a user submits a blank signup form
+    Then no account is created
+    And they receive error messages
+
+  Scenario: if a user enters an invalid email address
+    When a user submits an invalid email address
     Then their account is not created
     And they receive an error message
