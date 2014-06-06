19efbb592954c75cba6f814ea7de16c690d15a2e
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-06 14:27:01 -0500

    finished last three cucumber signup validation scenarios

--- features/step_definitions/signup_errors_steps.rb
+++ features/step_definitions/signup_errors_steps.rb
@@ -87,3 +87,69 @@ end
 Then(/^they receive a first name too short error message$/) do
   page.should have_content 'is too short (minimum is 2 characters)'
 end
+
+When(/^a user submits a single character last name$/) do
+  @user_count = User.count
+  visit('/')
+  fill_in 'user_email', :with => 'email@email.com'
+  click_button('Create Account')
+  user = { first_name: 'First',
+           last_name: 'L',
+           location: 'Smallville, KS',
+           password: 'password',
+           password_confirmation: 'password' }
+  user.each { |field,value| fill_in "user_#{field}", :with => value }
+  click_button('Create Your Account')
+end
+
+Then(/^their new account is not created$/) do
+  User.count.should == @user_count
+end
+
+Then(/^they receive a last name too short error message$/) do
+  page.should have_content 'is too short (minimum is 2 characters)'
+end
+
+When(/^a user submits a password under eight characters$/) do
+  @user_count = User.count
+  visit('/')
+  fill_in 'user_email', :with => 'email@email.com'
+  click_button('Create Account')
+  user = { first_name: 'First',
+           last_name: 'Last',
+           location: 'Smallville, KS',
+           password: 'passwor',
+           password_confirmation: 'passwor' }
+  user.each { |field,value| fill_in "user_#{field}", :with => value }
+  click_button('Create Your Account')
+end
+
+Then(/^their user account is not created$/) do
+  User.count.should == @user_count
+end
+
+Then(/^they receive a password too short error message$/) do
+  page.should have_content 'must be at least 8 characters'
+end
+
+When(/^a user submits a password confirmation that does not match their password$/) do
+  @user_count = User.count
+  visit('/')
+  fill_in 'user_email', :with => 'email@email.com'
+  click_button('Create Account')
+  user = { first_name: 'First',
+           last_name: 'Last',
+           location: 'Smallville, KS',
+           password: 'password',
+           password_confirmation: 'passwordd' }
+  user.each { |field,value| fill_in "user_#{field}", :with => value }
+  click_button('Create Your Account')
+end
+
+Then(/^a user account is not created$/) do
+  User.count.should == @user_count
+end
+
+Then(/^they receive a does not match password error message$/) do
+  page.should have_content "doesn't match Password"
+end
