de0d445b500c9552630d6bb10f146dfcb4adcc67
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-10 10:49:19 -0500

    account cancel feature and scenarios working

--- features/step_definitions/cancel_account_steps.rb
+++ features/step_definitions/cancel_account_steps.rb
@@ -1,35 +1,25 @@
 Then(/^their account page should have a 'cancel your account' link$/) do
-  @user = User.create(email: 'email@email.com',
-                      first_name: 'First',
-                      last_name: 'Last',
-                      location: 'Smallville, KS',
-                      password: 'password',
-                      password_confirmation: 'password')
-  visit("/users/#{@user.id}")
+  visit('/')
+  fill_in 'user_email', :with => 'email@email.com'
+  click_button('Create Account')
+  user = { first_name: 'First',
+           last_name: 'Last',
+           location: 'Smallville, KS',
+           password: 'password',
+           password_confirmation: 'password' }
+  user.each { |field,value| fill_in "user_#{field}", :with => value }
+  click_button('Create Your Account')
+  @id = User.last.id
+  @count = User.count
   page.should have_content "cancel your account"
 end
 
 When(/^they click on 'cancel your account' on their user page they should see an 'are you sure' message$/) do
-  @user2 = User.create(email: 'email2@email.com',
-                       first_name: 'First',
-                       last_name: 'Last',
-                       location: 'Smallville, KS',
-                       password: 'password',
-                       password_confirmation: 'password')
-  visit("/users/#{@user2.id}")
   click_link('cancel your account')
   page.should have_content "Are you sure that you want to close your account?"
 end
 
 When(/^they click 'yes' their account is destroyed$/) do
-  @user3 = User.create(email: 'email3@email.com',
-                       first_name: 'First',
-                       last_name: 'Last',
-                       location: 'Smallville, KS',
-                       password: 'password',
-                       password_confirmation: 'password')
-  visit("/users/#{@user3.id}")
-  click_link('cancel your account')
   click_link('YES')
-  User.find(@user3.id).should == nil
+  User.count.should < @count
 end
