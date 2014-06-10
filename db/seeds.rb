4764f5a449a5e42d00e0ef218bba15130c843941
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-10 15:41:43 -0500

    updated seeds.rb after resetting and updating db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 100.times do |n|
-  User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}")
-  Suggestion.create(owner_id: n + 1, target: Faker::Company.name, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, score: rand(1..100), local: [true,false].shuffle[0])
-  Comment.create(owner_id: rand(1..100), suggestion_id: rand(1..100), vote: [1,-1].shuffle[0], body: Faker::Lorem.paragraph)
+  User.create(email: Faker::Internet.email,
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
+              password: 'password',
+              password_confirmation: 'password')
+  Suggestion.create(user_id: n + 1,
+                    target: Faker::Company.name,
+                    title: Faker::Lorem.sentence,
+                    body: Faker::Lorem.paragraph,
+                    score: rand(1..100),
+                    local: [true,false].shuffle[0])
+  Comment.create(user_id: rand(1..100),
+                 suggestion_id: rand(1..100),
+                 vote: [1,-1].shuffle[0],
+                 body: Faker::Lorem.paragraph)
 end
