e07b7fa764f4e98028c6caaab21434816347e101
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-10 16:50:51 -0500

    updated suggestions#create based on new db fields

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -6,7 +6,7 @@ class SuggestionsController < ApplicationController
   end
 
   def create
-    @suggestion = Suggestion.create(owner_id: 1, target: params[:suggestion][:target], title: params[:suggestion][:title], body: params[:suggestion][:body], local: params[:suggestion][:local])
+    @suggestion = Suggestion.create(user_id: session[:current_user_id], target: params[:suggestion][:target], title: params[:suggestion][:title], body: params[:suggestion][:body], local: params[:suggestion][:local])
     redirect_to "/suggestions/#{@suggestion.id}"
   end
 
