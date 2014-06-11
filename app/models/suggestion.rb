64d908a3a32e14eede48bd9e9fda60fed4f7d4c7
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-11 11:48:52 -0500

    added Suggestion model validations

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,4 +1,12 @@
 class Suggestion < ActiveRecord::Base
+
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
+
+  validates :user_id, presence: { message: 'no current user' }
+  validates :target, presence: { message: 'required' }
+  validates :title, presence: { message: 'required' }
+  validates :body, presence: { message: 'required' }
+  validates :score, presence: { message: 'required' }
+
 end
