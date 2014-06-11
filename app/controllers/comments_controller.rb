bf8a0bc7baddf576af4223f749fe1b794be3fee1
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-11 11:51:36 -0500

    updated comments#create to reflect db changes and sessions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(owner_id: 1, suggestion_id: params[:suggestion_id], body: params[:comment][:body], vote: params[:comment][:vote])
+    @comment = Comment.create(user_id: session[:current_user_id],
+                              suggestion_id: params[:suggestion_id],
+                              body: params[:comment][:body],
+                              vote: params[:comment][:vote])
     redirect_to "/suggestions/#{@comment.suggestion_id}"
   end
 end
