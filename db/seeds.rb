fcbb6a235c5875babf48ecce0622e2c4c37b8bea
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-12 11:28:25 -0500

    updated seeds.rb to create more robust development db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-100.times do |n|
+100.times do
   User.create(email: Faker::Internet.email,
               first_name: Faker::Name.first_name,
               last_name: Faker::Name.last_name,
@@ -14,19 +14,23 @@
               password: 'password',
               password_confirmation: 'password')
 end
-100.times do |n|
-  Suggestion.create(user_id: n + 1,
-                    target: Faker::Company.name,
-                    title: Faker::Lorem.sentence,
-                    body: Faker::Lorem.paragraph,
-                    score: 1,
-                    local: [true,false].shuffle[0])
+4.times do
+  100.times do |n|
+    Suggestion.create(user_id: n + 1,
+                      target: Faker::Company.name,
+                      title: Faker::Lorem.sentence,
+                      body: Faker::Lorem.paragraph,
+                      score: 1,
+                      local: [true,false].shuffle[0])
+  end
 end
-100.times do |n|
-  Comment.create(user_id: rand(1..100),
-                 suggestion_id: rand(1..100),
-                 vote: [1,-1].shuffle[0],
-                 body: Faker::Lorem.paragraph)
+5.times do
+  100.times do |n|
+    Comment.create(user_id: rand(1..100),
+                   suggestion_id: n + 1,
+                   vote: [1,-1].shuffle[0],
+                   body: Faker::Lorem.paragraph)
+  end
 end
 
 Suggestion.all.each { |s| s.update_score! }
