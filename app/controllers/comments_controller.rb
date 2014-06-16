6ee9bc259b13f4512caf0ee18b2f34cf84a4b385
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-16 10:58:59 -0500

    added error handling for Comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(user_id: session[:current_user_id],
-                              suggestion_id: params[:suggestion_id],
-                              body: params[:comment][:body],
-                              vote: params[:comment][:vote])
-    Suggestion.find(params[:suggestion_id]).update_score!
-    redirect_to "/suggestions/#{@comment.suggestion_id}"
+    @comment = Comment.new(user_id: session[:current_user_id],
+                           suggestion_id: params[:suggestion_id],
+                           body: params[:comment][:body],
+                           vote: params[:comment][:vote])
+    if @comment.save
+      Suggestion.find(params[:suggestion_id]).update_score!
+      redirect_to "/suggestions/#{@comment.suggestion_id}"
+    else
+      @errors = @comment.errors
+      render('comments/new')
+    end
   end
 end
