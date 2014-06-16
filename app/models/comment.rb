5acc6addf6e26c76192e66601e9606eeb87fba8e
Geoff Crittenden <geoffcritt@hotmail.com>
2014-06-16 10:58:23 -0500

    added validations for Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
+
   belongs_to :suggestion
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+
+  validates :user_id, presence: { message: 'no current user' }
+  validates :suggestion_id, presence: { message: 'no current user' }
+  validates :body, presence: { message: 'required' },
+                   length: { minimum: 16,
+                             too_short: 'comment too short' }
+
 end
